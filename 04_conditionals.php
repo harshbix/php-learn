eb04fec06e97d601fcee2406e98deb3d93df7256
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-04 06:32:20 -0800

    Update 04_conditionals.php
    added knowledge about else if and its conditions

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -1,11 +1,25 @@
 <?php
 //if else syntax
- $age = 5;
+//  $age = 5;
 
-if ($age >= 18) {
-    # code...
-    echo 'You are old enough to vote';
-} else 
+// if ($age >= 18) {
+//     # code...
+//     echo 'You are old enough to vote';
+// } else 
+// {
+//     echo 'You are not old enough to vote';
+// }
+
+$time = date("H");
+
+if($time < 12)
 {
-    echo 'You are not old enough to vote';
+    echo 'Good Morning';
+}
+elseif ($time < 17) {
+    echo 'Good Afternoon';
+}
+else
+{
+    echo 'Good Evening';
 }
