95280fd2baa453824d251220df10a0dc40095a0c
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-04 06:44:28 -0800

    Update 04_conditionals.php
    More about ternary conditionals and if no second condition

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -10,16 +10,36 @@
 //     echo 'You are not old enough to vote';
 // }
 
-$time = date("H");
+// $time = date("H");
 
-if($time < 12)
-{
-    echo 'Good Morning';
-}
-elseif ($time < 17) {
-    echo 'Good Afternoon';
-}
-else
-{
-    echo 'Good Evening';
-}
+// if($time < 12)
+// {
+//     echo 'Good Morning';
+// }
+// elseif ($time < 17) {
+//     echo 'Good Afternoon';
+// }
+// else
+// {
+//     echo 'Good Evening';
+// }
+
+$posts = ['first post'];
+
+// if(!empty($posts))
+// {
+//     echo true;
+// }else {
+//     echo 'No Posts';
+// }
+
+//ternary conditionals 
+
+echo !empty($posts) ? $posts[0] : 'No Posts';
+
+// if no second condition
+echo !empty($posts) ? $posts[0] : null;
+
+//or use double question mark
+
+echo $posts[0] ?? null;
