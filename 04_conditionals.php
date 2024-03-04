ec2cc5cbb2bd595fe2dc10c6cc57afe5c687298f
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-04 06:25:28 -0800

    Update 04_conditionals.php
    first add if condition, then learn the else statement. and how it is used.

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -1,1 +1,11 @@
-hello world
+<?php
+//if else syntax
+ $age = 5;
+
+if ($age >= 18) {
+    # code...
+    echo 'You are old enough to vote';
+} else 
+{
+    echo 'You are not old enough to vote';
+}
