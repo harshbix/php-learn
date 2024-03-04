5fa3afe0100c8616026ee486d66cafac5040ebff
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-04 06:04:11 -0800

    Update 03_arrays.php
    added types of array

--- 03_arrays.php
+++ 03_arrays.php
@@ -1,10 +1,53 @@
 <?php
-// array is a data type or data structure that holds multiple value
-//syntax
-$fruits = ['apple', 'mango', 'banana'];
-$numbers = array(2, 6, 7, 10);
+// ------------ARRAYS------------
+//  array is a data type or data structure that holds multiple value
+// syntax
+// $fruits = ['apple', 'mango', 'banana'];
+// $numbers = array(2, 6, 7, 10);
 
-// var_dump($fruits);
-var_dump($numbers)
+// // var_dump($fruits);
+// var_dump($numbers);
+
+/*
+---------TYPES OF ARRAY-----------
+1. Indexed Array
+2. Associative array
+3. Multidimensional array
+*/ 
+
+// Indexed array syntax
+
+$students = ['Junior', 'Faith', 'Yvonne', 'Janeth'];
+// print_r ($students);
+
+//Associative array syntax
+
+$hex = [
+    'red' => '#000ff',
+    'blue' => 'ff0000',
+    'green' => '00ff00'
+];
+// print_r ($hex['blue']);
+
+// multidimensional array syntax
+$people = 
+[
+    [
+        'first_name' = 'Junior',
+        'company' = 'HL4L',
+        'adress' = 'Uyole'
+    ],
+    [
+        'first_name' = 'Salvatory',
+        'company' = 'THE BIG 6',
+        'adress' = 'Tunduma'
+    ],
+    [
+        'first_name' = 'Owen',
+        'company' = 'Mahenge Group, LLC',
+        'adress' = 'Mlowo'
+    ]
+]
+
 
 ?>
