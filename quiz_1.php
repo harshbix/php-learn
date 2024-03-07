b97d2e6724c8b4c075e2eef872f1189be99ff759
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-06 20:49:09 -0800

    Update quiz_1.php
    solved the second question.

--- quiz_1.php
+++ quiz_1.php
@@ -9,13 +9,33 @@
 
 /*
     2. conditional logic:
-        - develop a php program that prompts the user to enter their age 
-        Based on their age, use conditional statements to output different
-        messages. For example if the age is below 18 display "You are a minor"
-        if between 18 and 65 display "you are an adult" and if above 65 display
-        "you are a senior citizen"    
+        - develop a php program that prompts the user to enter 
+        their age 
+        Based on their age, use conditional statements to
+        output different messages.
+        For example if the age is below 
+        18 display "You are a minor"
+        if between 18 and 65 display "you are an adult"
+         and if above 65 display "you are a senior citizen"    
 */
-
+$age = 78;
+if($age < 18)
+{
+    echo "You are a minor";
+}
+elseif ($age < 65 && $age > 18)
+{
+    echo "you are an adult";
+}
+elseif($age >= 65)
+{
+    echo "you are a senior citizen";
+}
+else
+{
+    echo "invalid input";
+}
+echo "</br>";
 /*
     3.Variable Operations:
         - Create a php script that initializes two variables
