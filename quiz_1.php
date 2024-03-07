494bb2dcea9fdd961c0fa3c1ad6b5408b868025c
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-06 19:18:32 -0800

    Update quiz_1.php
    added some few questions from what i learned

---
+++ quiz_1.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+    1. array manipulation.
+        -write a php script that takes an array of numbers as input 
+        create a new array containing only the even numbers from the 
+        original array, and then display the result.
+*/
+
+/*
+    2. conditional logic:
+        - develop a php program that prompts the user to enter their age 
+        Based on their age, use conditional statements to output different
+        messages. For example if the age is below 18 display "You are a minor"
+        if between 18 and 65 display "you are an adult" and if above 65 display
+        "you are a senior citizen"    
+*/
+
+/*
+    3.Variable Operations:
+        - Create a php script that initializes two variables
+        one conataining a string and the other a number.
+        concatinate the string with the number and display 
+        the result. Then perfom a mathematical operation on the
+        number (eg square it) and display the updated value.
+*/
+?>
