aa6a6fe501a6bfcddcf5c66aed4eb4933b5ad2f7
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-03-07 11:16:57 -0800

    Create 05_loops.php

---
+++ 05_loops.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+for ( initialize; condition ;increrment) { 
+    # code...
+}
+*/
+
+// for($j = 1 ; $j < 11; $j++)
+// {
+//     echo $j;
+// }
+while ( $a <= 10) {
+    # code...
+    echo "number $a ". "</br>";
+    $a++;
+}
+?>
